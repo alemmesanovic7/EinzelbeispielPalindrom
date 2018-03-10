a0f2f29d4aa06160356f9b2ab0521a39e3504360
alemmes <alemmes@edu.aau.at>
2018-03-10 12:38:07 +0100

    commit1
    .setOnClickListener
    String palindromString
    
    created.

--- app/src/main/java/com/example/alem/einzelbeispielpalindrom/MainActivity.java
+++ app/src/main/java/com/example/alem/einzelbeispielpalindrom/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.alem.einzelbeispielpalindrom;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText input;
+    Button button;
+    EditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText) findViewById(R.id.inputField);
+        button = (Button) findViewById(R.id.buttonCheckString);
+        result = (EditText) findViewById(R.id.ergebnis);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String palindromString = result.getText().toString();
+
+            }
+        });
     }
 }
+
+
